1c6556380c46dc1e03aa9e94e4e4fe60b843c700
andresgr98 <gomezand15@gmail.com>
2024-09-30 20:56:01 +0200

    Create initial files for modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,23 +3,30 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { configuration } from './config/config'
+import { configuration } from './config/config';
 import { DummyModule } from './dummy/dummy.module';
+import { GymModule } from './gym/gym.module';
+import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [configuration]
+      load: [configuration],
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => configService.get('database'),
+      useFactory: (configService: ConfigService) =>
+        configService.get('database'),
       inject: [ConfigService],
     }),
     DummyModule,
+    GymModule,
+    UserModule,
+    PostModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
