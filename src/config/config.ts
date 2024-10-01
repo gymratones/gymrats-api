a8b30023fb70db5861994b79db881e736b6688b9
andresgr98 <gomezand15@gmail.com>
2024-10-01 21:24:02 +0200

    Configure migrations. Remove eslint. Test migration

--- src/config/config.ts
+++ src/config/config.ts
@@ -10,6 +10,6 @@ export const configuration = () => ({
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        synchronize: process.env.NODE_ENV !== 'production',
+        synchronize: false,
     } as TypeOrmModuleOptions,
 });
